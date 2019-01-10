a08b4ba7bed38c89c9141cca8f0a871d8246d7bb
28cmm <joshuafang1998@gmail.com>
2019-01-10 11:06:21 -0800

    Initial Commit

---
+++ patient & Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  patient & Doctor
+//
+//  Created by Yilei Huang on 2019-01-10.
+//  Copyright © 2019 Joshua Fanng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
